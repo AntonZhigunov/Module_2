74d10a78d5063e1ae80d79a9be8227db6607bfb5
AntonZhigunov <azhigunov79@gmail.com>
2024-11-02 23:59:21 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,15 @@
+my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
+a = len(my_list)
+#print(a)
+#number1 = my_list[0]
+#if number1 >= 0:
+#    print(number1)
+#else:
+#   print()
+x = - 1
+while a > x:
+    x = x + 1
+    if my_list[x] > 0:
+        print(my_list[x])
+#    elif my_list[x] < 0:
+#        break
